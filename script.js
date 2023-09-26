d87a6f0aa7e5896c89eb77e74efb6d8d66d6a291
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-09-26 12:18:22 +0600

    Constraint Validation API part done

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Code Starts Here..
+
+const form = document.querySelector('#form');
+const message = document.querySelector('#message');
+
+
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  validateForm();
+});
+
+
+
+
+let isValid = false;
+
+const validateForm = () => {
+  isValid = form.checkValidity();
+  console.log(isValid);
+  if (!isValid) {
+    message.textContent = 'Please fill out all fields';
+    message.style.color = 'red';
+  }
+}
