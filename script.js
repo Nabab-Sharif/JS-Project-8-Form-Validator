0af9b7b58de9cb8a2ad16533207a6ff69cda58ea
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-09-26 12:53:41 +0600

    Validate password and show final Object . Wrap up!! part done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const form = document.querySelector('#form');
 const message = document.querySelector('#message');
-
+const password1 = document.querySelector('#password1')
+const password2 = document.querySelector('#password2')
 
 
 
@@ -9,18 +10,54 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   validateForm();
+
+  if (isValid && passwordMatch) {
+    const user = {
+      name: form.name.value,
+      email: form.email.value,
+      phone: form.phone.value,
+      website: form.website.value,
+      password: form.password.value,
+    }
+    console.log(user)
+  }
 });
 
 
 
 
 let isValid = false;
+let passwordMatch = false;
 
 const validateForm = () => {
   isValid = form.checkValidity();
-  console.log(isValid);
+
+
   if (!isValid) {
     message.textContent = 'Please fill out all fields';
     message.style.color = 'red';
+  } else {
+    if (password1.value === password2.value) {
+      passwordMatch = true;
+
+      password1.style.borderColor = 'green';
+      password2.style.borderColor = 'green';
+    } else {
+      passwordMatch = false;
+      message.textContent = 'Make sure passwords match';
+      message.style.color = 'red';
+
+      password1.style.borderColor = 'red';
+      password2.style.borderColor = 'red';
+      return;
+    }
+
+    if (isValid && passwordMatch) {
+      message.textContent = 'Successfully Registered';
+      message.style.color = 'green';
+    }
+
   }
+
+
 }
